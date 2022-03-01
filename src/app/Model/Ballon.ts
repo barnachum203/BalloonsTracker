f0d65aaa26a713552e70509848331b159648ed53
barnachum <barnachum203@gmail.com>
2022-03-01 14:51:07 +0200

    Ballon model

---
+++ src/app/Model/Ballon.ts
@@ -0,0 +1,29 @@
+export class Ballon {
+  constructor(
+    public name: string, //unique
+    public type: Type,
+    public description: string,
+    public color: Color,
+    public position: Position,
+    public id: string
+  ) {}
+}
+export enum Type {
+  small = 'small',
+  medium = 'medium',
+  big = 'big',
+  double = 'double',
+}
+
+export enum Color {
+  red = 'red',
+  blue = 'blue',
+  black = 'black',
+  white = 'white',
+}
+
+export interface Position{
+    latitude: number,
+    longitude: number,
+    attitude: number
+}
