e0b4d28873f6234cdf82f7d3bb1e81453c0266fe
barnachum <barnachum203@gmail.com>
2022-03-02 17:11:22 +0200

    create and edit implementation

--- src/app/components/menu/ballon-dialog/ballon-dialog.component.ts
+++ src/app/components/menu/ballon-dialog/ballon-dialog.component.ts
@@ -1,16 +1,20 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Form, FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import {
   MatDialogRef,
   MAT_DIALOG_DATA,
   MatDialog,
 } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
-import { Ballon } from 'src/app/Model/Ballon';
+import { Store } from '@ngrx/store';
+import { Observable, pipe } from 'rxjs';
+import { Ballon, Color, Type, BallonPosition } from 'src/app/Model/Ballon';
 import { QuestionService } from 'src/app/shared/question.service';
 import { QuestionBase } from '../../shared/forms/question-base';
-
+import * as MapSelectors from '../store/map.selectors';
+import * as MapActions from '../store/map.actions';
 import * as IDialog from './dialog.types';
+import { MapState } from '../store/map.models';
+import { createBallonRequest } from '../store/map.actions';
 
 export interface DialogData {
   ballon?: Ballon;
@@ -28,31 +32,47 @@ export class BallonDialogComponent implements OnInit {
   questions$?: Observable<QuestionBase<any>[]>;
   title = '';
   mode = '';
+  buttonName = '';
 
   constructor(
     public dialogRef: MatDialogRef<BallonDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     public dialog: MatDialog,
+    private store: Store<MapState>,
     private questionService: QuestionService
   ) {
     this.title = this.data.dialogType;
 
-    if (data.dialogType == IDialog.EDIT) {
-      this.mode = 'EDIT';
-      this.ballon = this.data.ballon;
-      this.title = `Edit Air Ballon: ${this.ballon?.name}`;
-
-      if (this.data.ballon)
+    switch (data.dialogType) {
+      case IDialog.EDIT:
+        this.mode = IDialog.EDIT;
+        this.ballon = this.data.ballon;
+        this.title = `Edit Air Ballon: ${this.ballon?.name}`;
+        this.buttonName = 'Update';
         this.questions$ = this.questionService.getEditBallonQuestions(
-          this.data.ballon
+          this.data.ballon!
         );
-        
-    } else {
-      this.mode = 'NEW';
-      this.title = 'Add new Air Ballon';
-      this.questions$ = questionService.getAddBallonQuestions();
+        break;
+
+      case IDialog.ADD_NEW:
+        this.mode = IDialog.ADD_NEW;
+        this.title = 'Add new Air Ballon';
+        this.buttonName = 'Create';
+        this.questions$ = questionService.getAddBallonQuestions();
+        break;
+
+      default:
+        break;
     }
   }
 
   ngOnInit(): void {}
+
+  onSave() {
+    console.log('onSave()');
+  }
+
+  onCreate() {
+    console.log('onCreate()');
+  }
 }
