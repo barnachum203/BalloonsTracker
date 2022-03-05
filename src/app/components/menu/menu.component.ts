2128205a67d880a3358c5ea756c0047f10ec753d
barnachum203 <barnachum203@gmail.com>
2022-03-06 00:55:06 +0200

    get list of ballons from store

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -15,37 +15,51 @@ import * as MapSelectors from './store/map.selectors';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
   // @Input() currentBallon!: Ballon;
 
-  ballons!: Ballon[];
-  ballons$ = this.store.select(MapSelectors.selectMapBallons)
+  // ballons!: Ballon[];
+
+  ballons$ = this.store.select(MapSelectors.selectMapBallons);
+  storeData: Ballon[] | undefined;
+  subscribtion;
+
   constructor(
     public dialog: MatDialog,
     private route: Router,
-    // ballonService: BallonService,
     private store: Store
   ) {
-    this.getBallons()
+    //Check if data exist in store before send http request.
+    this.subscribtion = this.ballons$.subscribe((data) => {
+      this.storeData = data;
+      //if data isn't exist - send request
+      if (!this.storeData) {
+        this.getBallons();
+      }
+    });
+
     // ballonService.getAllBallons().subscribe((data) => {
     //   this.ballons = data;
     //   console.log(this.ballons);
     // });
   }
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
 
   ngOnInit(): void {
     console.log();
-
   }
 
-  getBallons(){
+  getBallons() {
     //TODO: get ballons once
-      this.store.dispatch(MapActions.getBallons())
+    this.store.dispatch(MapActions.getBallons());
   }
 
   openCreationDialog() {
     const dialogRef = this.dialog.open(BallonDialogComponent, {
       width: '440px',
+      height: '700px',
       disableClose: false,
       data: { dialogType: IDialog.ADD_NEW },
     });
@@ -54,6 +68,7 @@ export class MenuComponent implements OnInit {
   openEditDialog(ballon: any) {
     const dialogRef = this.dialog.open(BallonDialogComponent, {
       width: '440px',
+      height: '700px',
       disableClose: false,
       data: { dialogType: IDialog.EDIT, ballon: ballon },
     });
