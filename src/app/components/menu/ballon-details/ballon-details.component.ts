a10f5eff52f671d50556342303e4ffe0f0367461
barnachum <barnachum203@gmail.com>
2022-03-06 12:07:28 +0200

    get ballon details fro the store

--- src/app/components/menu/ballon-details/ballon-details.component.ts
+++ src/app/components/menu/ballon-details/ballon-details.component.ts
@@ -1,49 +1,46 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { Ballon, BallonPosition } from 'src/app/Model/Ballon';
-import { BallonService } from 'src/app/services/ballon.service';
+import * as MapSelectors from '../store/map.selectors';
 
 @Component({
   selector: 'app-ballon-details',
   templateUrl: './ballon-details.component.html',
-  styleUrls: ['./ballon-details.component.css']
+  styleUrls: ['./ballon-details.component.css'],
 })
-export class BallonDetailsComponent implements OnInit {
+export class BallonDetailsComponent implements OnInit, OnDestroy {
+  ballon!: Ballon | undefined;
 
-  ballons!: Ballon[];
-  ballon: Ballon | undefined;
+  ballons$ = this.store.select(MapSelectors.selectMapBallons);
+  subscribtion: Subscription = new Subscription();
+
   position: BallonPosition | undefined;
-  id!: number;
-  constructor(private route: Router, private activatedRoute: ActivatedRoute,
-   private ballonService: BallonService) {
-     this.ballonService.getAllBallons().subscribe((data) => {
-      this.ballons = data;
-      console.log(this.ballons);
-      
-     });
-    }
+  id!: string;
+
+  constructor(
+    private route: Router,
+    private activatedRoute: ActivatedRoute,
+    private store: Store
+  ) {}
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.id = params.id;
-      //TODO: get ballon by id live form server / NgRx
-      this.ballon = this.ballons.find(element => element.id?.toString() == this.id.toString()) 
-      this.position = this.ballon?.position;
-      console.log(this.ballon);
-      
-    })
+    });
+    this.subscribtion = this.ballons$.subscribe((data) => {
+      this.ballon = data?.find((e) => e.id == this.id);
+    });
   }
 
-  getBallonById(){
-
-  }
-  backToMenu(){
+  getBallonById() {}
+  backToMenu() {
     this.route.navigate(['home']);
   }
 
-  onSave(){
-
-  }
-
+  onSave() {}
 }
