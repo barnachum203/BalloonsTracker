f77840c13a89a225ba632c4180a446133d9def99
barnachum203 <barnachum203@gmail.com>
2022-03-13 14:56:37 +0200

    naming

--- backend/balloons-microservice/src/routes/balloons.routes.ts
+++ backend/balloons-microservice/src/routes/balloons.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as balloonController from '../controllers/balloonController';
 const router = express.Router();
 
-router.get('/:uid', balloonController.getBalloons);
+router.get('/:uid', balloonController.getBalloonsForUser);
 router.get('/one/:bid', balloonController.getBalloonById);
 router.put('/:bid', balloonController.updateBalloon);
 router.delete('/:bid', balloonController.deleteBalloon);
