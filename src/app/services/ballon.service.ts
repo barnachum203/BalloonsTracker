6323e86ad93e2c0c4d2b2b59aae11c066f7efdb2
barnachum <barnachum203@gmail.com>
2022-03-14 17:46:29 +0200

    added headers and storage changed id to _id, ballon to balloon

--- src/app/services/ballon.service.ts
+++ src/app/services/ballon.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Ballon } from '../Model/Ballon';
+import { TokenStorageService } from '../shared/token-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +12,23 @@ export class BallonService {
 
   apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private storage: TokenStorageService) {}
 
   getAllBallons(): Observable<Ballon[]> {
-    return this.http.get<Ballon[]>(this.apiUrl + '/ballon');
+    return this.http.get<Ballon[]>(this.apiUrl + '/balloon');
   }
 
   getBallonById(id: any): Ballon {
     throw new Error('Method not implemented.');
   }
 
-  updateBallon(ballon: Ballon): Observable<Ballon> {
-    return this.http.put<Ballon>(this.apiUrl + '/ballon', { ballon });
+  updateBallon(balloon: Ballon): Observable<Ballon> {
+    console.log("update balloon called", balloon);
+    
+    return this.http.put<Ballon>(this.apiUrl + '/balloon', { balloon: balloon, bid:balloon._id });
   }
-  
-  createBallon(ballon: Ballon): Observable<Ballon> {
-    return this.http.post<Ballon>(this.apiUrl + '/ballon', { ballon });
+
+  createBallon(balloon: Ballon): Observable<Ballon> {
+    return this.http.post<Ballon>(this.apiUrl + '/balloon', { balloon });
   }
 }
