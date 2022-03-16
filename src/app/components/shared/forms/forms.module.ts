b5b26ac9a6974bdb18830ce3e9e90c62da3835f9
barnachum <barnachum203@gmail.com>
2022-03-16 16:39:47 +0200

    naming

--- src/app/components/shared/forms/forms.module.ts
+++ src/app/components/shared/forms/forms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DynamicFormFieldComponent } from './dynamic-form-field/dynamic-form-field.component';
+import { DynamicFormFieldComponent } from './dynamic-form-question/dynamic-form-field.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
