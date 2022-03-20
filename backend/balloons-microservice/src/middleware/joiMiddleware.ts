95dc30796cd54ecadfbf5e5e8faed7571183a7dd
barnachum203 <barnachum203@gmail.com>
2022-03-20 11:04:58 +0200

    destruct balloon

--- backend/balloons-microservice/src/middleware/joiMiddleware.ts
+++ backend/balloons-microservice/src/middleware/joiMiddleware.ts
@@ -3,18 +3,16 @@ import { logger } from '../utils/logger';
 
 export default function joiMiddleware(schema) {
   return (req: Request, res: Response, next: NextFunction) => {
-    // TODO: destruct balloon
-    const { error } = schema.validate(req.body['balloon']);
-
+    const {balloon} = req.body;
+    const { error } = schema.validate(balloon);
     const valid = error == null;
+
     logger.info('VALIDATION: ' + valid);
 
     if (valid) {
       next();
     } else {
       const { details } = error;
-      // console.log(error);
-
       const message = details.map((i) => i.message).join(',');
 
       logger.error('error', message);
