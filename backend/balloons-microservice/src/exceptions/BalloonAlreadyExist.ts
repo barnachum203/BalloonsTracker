5afee945928c0c590799555280b3e5a3664b1ee9
barnachum203 <barnachum203@gmail.com>
2022-03-21 17:27:54 +0200

    update balloon name exists status code from 401 to 409

--- backend/balloons-microservice/src/exceptions/BalloonAlreadyExist.ts
+++ backend/balloons-microservice/src/exceptions/BalloonAlreadyExist.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from "../enums/HttpStatusCode";
 import { BaseError } from "./BaseError";
 export class BalloonAlreadyExist extends BaseError {
-    constructor(message, statusCode = HttpStatusCode.UNAUTHORIZED) {
+    constructor(message, statusCode = HttpStatusCode.CONFLICT) {
       super(message, statusCode);
       this.name = 'BalloonAlreadyExist';
     }
