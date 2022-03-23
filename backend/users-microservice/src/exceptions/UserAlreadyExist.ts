ce965bac3c1951eb3d72b80480da7b05dcd9dcca
barnachum <barnachum203@gmail.com>
2022-03-23 11:11:06 +0200

    change status code

--- backend/users-microservice/src/exceptions/UserAlreadyExist.ts
+++ backend/users-microservice/src/exceptions/UserAlreadyExist.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from "../enums/HttpStatusCode";
 import { BaseError } from "./BaseError";
 export class UserAlreadyExist extends BaseError {
-    constructor(message, statusCode = HttpStatusCode.UNAUTHORIZED) {
+    constructor(message, statusCode = HttpStatusCode.CONFLICT) {
       super(message, statusCode);
       this.name = 'UserAlreadyExist';
     }
