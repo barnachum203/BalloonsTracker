e4cad4ee503942722890549ca616f6ee461fb9f8
barnachum <barnachum203@gmail.com>
2022-04-04 14:58:02 +0300

    Added Facade

---
+++ src/app/components/login/store/auth.facade.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TokenStorageService } from 'src/app/shared/token-storage.service';
+
+import * as AuthActions from './auth.actions';
+import * as AuthSelectors from './auth.selectors';
+
+@Injectable()
+export class AuthFacade {
+  auth$ = this.store.select(AuthSelectors.selectAuth);
+  user$ = this.store.select(AuthSelectors.selectAuthUser);
+  isLoggedIn$ = this.store.select(AuthSelectors.selectIsLoggedIn);
+  isLoadingLogin$ = this.store.select(AuthSelectors.selectIsLoadingLogin);
+  hasLoginError$ = this.store.select(AuthSelectors.selectLoginError);
+
+  constructor(private store: Store, private storageService: TokenStorageService) {}
+
+  login(email: string, password: string) {
+    this.store.dispatch(AuthActions.loginRequest({ email, password }));
+  }
+
+  logout() {
+    this.storageService.signOut();
+    this.store.dispatch(AuthActions.logout());
+  }
+}
---
+++ src/app/components/menu/store/map.facade.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Ballon } from 'src/app/Model/Ballon';
+
+import * as MapActions from './map.actions';
+import * as MapSelectors from './map.selectors';
+
+@Injectable()
+export class MapFacade {
+  activeBalloon$ = this.store.select(MapSelectors.selectActiveBallon);
+  hasError$ = this.store.select(MapSelectors.selectMapError);
+  isLoadig$ = this.store.select(MapSelectors.selectIsLoading);
+  ballons$ = this.store.select(MapSelectors.selectMapBallons);
+  hasMapError$ = this.store.select(MapSelectors.selectMapError);
+  errorMessage$ = this.store.select(MapSelectors.selectMapErrorMessage);
+  error$ = this.store.select(MapSelectors.selectError);
+
+  constructor(private store: Store) {}
+
+  createBalloon(ballon: Ballon) {
+    this.store.dispatch(MapActions.createBallonRequest({ ballon }));
+  }
+
+  updateBalloon(ballon: Ballon) {
+    this.store.dispatch(MapActions.updateRequest({ ballon }));
+  }
+
+  getAllBaoons() {
+    this.store.dispatch(MapActions.getBallons());
+  }
+  setActiveBalloon(ballon: Ballon) {
+    this.store.dispatch(MapActions.activeBallon({ ballon }));
+  }
+
+  deactivateBalloon() {
+    this.store.dispatch(MapActions.unactiveBallon());
+  }
+}
